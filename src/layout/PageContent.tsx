2dd75869a0c0234a7f4482d465ad06d32db3a629
Lukas Woodtli <woodtli.lukas@gmail.com>
2022-03-07 17:44:31 +0100

    Upgrade to react-router 6

--- src/layout/PageContent.tsx
+++ src/layout/PageContent.tsx
@@ -1,18 +1,14 @@
-import {Route, Switch} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import DummyPage from "../pages/DummyPage";
 import React from "react";
 
 export function PageContent() {
     return (
-        <Switch>
-            <Route path='/' exact>
-                <MainPage/>
-            </Route>
-            <Route path='/page/:content'>
-                <DummyPage/>
-            </Route>
-        </Switch>
+        <Routes>
+            <Route path='/' element={<MainPage/>}/>
+            <Route path='/page/:content' element={<DummyPage/>}/>
+        </Routes>
     );
 }
 
